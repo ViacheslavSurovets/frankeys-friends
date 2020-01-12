a02916bfce0554c7f86df0fa62a49de271fcaa52
Viacheslav <surovetsviacheslav@gmail.com>
2020-01-12 20:22:57 +0300

    Updates

---
+++ precache-manifest.9ab5ec87440287b53c1fb4f8fa7e441d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69c8e202c0106d55fa40290b54147460",
+    "url": "/frankeys-friends/index.html"
+  },
+  {
+    "revision": "479439872ca8b9573b2e",
+    "url": "/frankeys-friends/static/css/main.6fff35bb.chunk.css"
+  },
+  {
+    "revision": "49a92672d311802857fc",
+    "url": "/frankeys-friends/static/js/2.c3776a56.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/frankeys-friends/static/js/2.c3776a56.chunk.js.LICENSE"
+  },
+  {
+    "revision": "479439872ca8b9573b2e",
+    "url": "/frankeys-friends/static/js/main.28fe06f9.chunk.js"
+  },
+  {
+    "revision": "269dd45607070f0aae99",
+    "url": "/frankeys-friends/static/js/runtime-main.57e50188.js"
+  }
+]);
